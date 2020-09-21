5987c2bfda16253a766d233803f38246107a554c
guijun13 <guilhermejun13.grillo@gmail.com>
2020-09-21 17:37:05 -0300

    get e post de mensagens em array com json-parser

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-app.get('/', function(request, response){
-  response.send('GET Hello World!');
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.post('/', function(request, response){
-  response.send('POST Hello World!');
-})
+// CRUD de uma envio de mensagens
 
-app.listen(port, function(){
-  console.log(`App rodando em http://localhost:3000`);
-})
+const mensagens = [
+  "Mensagem 1",
+  "Mensagem 2"
+];
+
+// Read all
+app.get('/mensagens', (req, res) => {
+  res.json(mensagens);
+});
+
+// Create
+app.post('/mensagens', (req, res) => {
+  // Obtendo a mensagem que foi recebida através do body da requisição
+  const mensagem = req.body.mensagem;
+  // Insiro a mensagem na lista de mensagens
+  mensagens.push(mensagem);
+
+  const id = mensagens.length - 1;
+
+  res.send(`Criar uma mensagem com o corpo: ${mensagem}`);
+  // Exibido o ID da mensagem, que no caso é o índice que ela foi adicionada
+  // res.send(`Criar uma mensagem com id: ${id}`);
+});
+
+// Read one
+app.get('/mensagens/:id', (req, res) => {
+  res.send('Exibe uma mensagem selecionada pelo id informado');
+});
+
+// Update one
+app.put('/mensagens/:id', (req, res) => {
+  res.send('Atualiza uma mensagem selecionada pelo id informado');
+});
+
+// Delete one
+app.delete('/mensagens/:id', (req, res) => {
+  res.send('Remove uma mensagem selecionada pelo id informado');
+});
+
+app.listen(port, () => {
+  console.log(`App rodando em http://localhost:${port}`);
+});
