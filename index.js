168b4ca5b630ef3c1fe74819fd3078d5cb0343fd
guijun13 <guilhermejun13.grillo@gmail.com>
2020-09-23 16:37:51 -0300

    codigo modif p/ conectar com banc dados mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -91,3 +92,66 @@ app.delete('/mensagens/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`App rodando em http://localhost:${port}`);
 });
+
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+// Criando e executando um bloco assincrono
+(async() => {
+
+  const connectionString = 'mongodb+srv://admin:guijun13AtlasPass@cluster0.xd7o3.mongodb.net/mongoOceanDataBase?retryWrites=true&w=majority';
+  
+  console.info('Conectando ao banco de dados MongoDB');
+  
+  const client = await mongodb.MongoClient.connect(connectionString, {
+    useUnifiedTopology: true,
+  });
+
+  console.info('Banco de dados conectado');
+
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  const jsonParser = bodyParser.json();
+  app.use(jsonParser);
+
+  app.get('/', (req, res) => {
+      res.send('Hello world com MongoDB!');
+  });
+
+  // Endpoints de envio de mensagens
+  // CRUD -> Create, Read (Read All e Read Single), Update and Delete
+  // CRUD -> Criar, Ler (Ler tudo e ler individualmente), atualizar e remover
+
+  const mensagens = [];
+
+  // Read All
+  app.get('/mensagens', (req, res) => {
+      res.json(mensagens.filter(Boolean));
+  });
+
+  // Create
+  app.post('/mensagens', (req, res) => {
+  });
+
+  // Read Single
+  app.get('/mensagens/:id', (req, res) => {
+  });
+
+  // Update
+  app.put('/mensagens/:id', (req, res) => {
+  });
+
+  // Delete
+  app.delete('/mensagens/:id', (req, res) => {
+  });
+
+  app.listen(port, () => {
+    console.log(`App rodando em http://localhost:${port}`);
+  });
+
+})(); // Fechando o bloco assíncrono
+
